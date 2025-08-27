5783d46431dcf878b8f1e6dadf7400c85b295a6e
Julian Dehne <julian.dehne@gmail.com>
2025-08-27 10:16:45 +0200

    added branching for both tracks

--- generation/generate_all.py
+++ generation/generate_all.py
@@ -9,7 +9,7 @@ from load_profiles import load_profile
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Generate the Curriculum files.") # required positional argument
-    parser.add_argument("--profile", default="science")
+    parser.add_argument("--profile", default="mnt")
     parser.add_argument("--generate", default="false")
     # optional
 
@@ -17,12 +17,12 @@ if __name__ == "__main__":
 
     print(f"{args.profile}, {args.generate}!")
 
-    print("collect components")
+    print("collecting components...")
     module_summaries, dependencies = process_components()
     if args.generate == "true":
         optimal_solution = create_curriculum_solver(dependencies)
     else:
         ## load profiles
         optimal_solution = load_profile(args.profile, dependencies)
-    print("generate ")
-    generate_curriculum(module_summaries, optimal_solution)
+    print("generating curriculum...")
+    generate_curriculum(module_summaries, optimal_solution, args.profile)
